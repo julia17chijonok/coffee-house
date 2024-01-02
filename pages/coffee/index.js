6e73914ef4585eddd9b16d6d02266a0248b30636
Julia <chijonok.julia17@gmail.com>
2024-01-03 00:28:47 +0300

    feat: implement calculation price

--- pages/coffee/index.js
+++ pages/coffee/index.js
@@ -26,7 +26,7 @@ let windowWidth = window.innerWidth;
 
 let currentCategory = 'coffee';
 let tabBlockItem = document.querySelectorAll('.menu-coffee__item');
-let arr = Array.from(tabBlockItem);
+let arr;
 
 tabItems.forEach((item) => {
     item.addEventListener('click', () => {
@@ -83,14 +83,7 @@ class MenuCard {
     }
 }
 
-// let request = fetch('./products.json');
-// let data = request.then(response => response.json()).then();
-
-// let filteredProducts = data.filter(product => product.category === `${currentCategory}`);
-
-// console.log(data);
-
-// request.then(response => response.json()).then(json => console.log(json));
+let dataProducts;
 
 function makePesponse() {
     fetch('./products.json')
@@ -108,14 +101,19 @@ function makePesponse() {
                 
                 ).render();
             }
-            hideTabs();
             arr = Array.from(tabBlockItem);
-            
+            hideTabs(); 
+            productsData = data;
+            return productsData;
         }
         
         createCard();
+        modalWindowClick();
+        console.log(arr[0]);
     });
 }
+
+console.log(dataProducts);
 
 makePesponse();
 
@@ -147,3 +145,113 @@ refreshBtn.addEventListener('click', () => {
     });
     refreshBtn.classList.add('invisible');
 });
+
+
+// MODAL WINDOW
+
+const modalWindow = document.querySelector('.modal-window__bg'),
+      modalDilog = document.querySelector('.modal-window__inner'),
+      modalCloseBtn = document.querySelector('.modal-window__close');
+
+let startPrice = 0;      
+
+function renderModalWindow(name, descr, price, src, addictives, sizes) {
+    let nameTab = document.querySelector('.modal-window__name'),
+        descrTab = document.querySelector('.modal-window__descr'),
+        priceTab = document.querySelector('.modal-window__price'),
+        imgTab = document.querySelector('.modal-window__img'),
+        addictivesTab = document.querySelectorAll('.modal-window-addictive'),
+        sizesTab = document.querySelectorAll('.modal-window__size');
+    nameTab.textContent = `${name}`;
+    descrTab.textContent = `${descr}`;
+    priceTab.textContent = `$${price}`;
+    imgTab.src = `${src}`;
+    for(let i = 0; i < addictivesTab.length; i++){
+        addictivesTab[i].textContent = `${addictives[i].name}`;
+    }
+    for(let i = 0; i < sizesTab.length; i++){
+        sizesTab[i].textContent = `${sizes[i].size}`;
+    }
+};
+
+
+function modalWindowClick() {
+    let arr = Array.from(tabBlockItem);
+    arr.forEach(tab => {
+        tab.addEventListener('click', () => {
+            deletePriceAddictives();
+            modalWindow.classList.add('active');
+            body.classList.add('no-scroll');
+            let id = tab.id;
+            startPrice = productsData[id].price;
+            renderModalWindow(productsData[id].name, 
+                productsData[id].description, 
+                productsData[id].price, 
+                productsData[id].src, 
+                productsData[id].additives,
+                Object.values(productsData[id].sizes));    
+            console.log(startPrice);
+            console.log(productsData[id].name);
+        });
+    });
+}
+
+modalCloseBtn.addEventListener('click', () => {
+    modalWindow.classList.remove('active');
+    body.classList.remove('no-scroll');
+});
+
+modalWindow.addEventListener('click', (e) => {
+    if (e.target == modalWindow) {
+        modalWindow.classList.remove('active');
+        body.classList.remove('no-scroll');
+    }
+});
+
+
+// MODAL WINDOW CALCULATE
+
+let priceSize = 0;
+let priceAdd = 0;
+
+const sizeBtns = document.querySelectorAll('.modal-window__sizes-btn'),
+      addBtns = document.querySelectorAll('.modal-window__add-btn');
+
+sizeBtns.forEach(sizeBtn => {
+    sizeBtn.addEventListener('click', () => {
+        sizeBtns.forEach(sizeBtn => {
+            sizeBtn.classList.remove('active');
+        });
+        sizeBtn.classList.add('active');
+        priceSize = 0;
+        priceSize += +(sizeBtn.getAttribute('price'));
+        let priceTab = document.querySelector('.modal-window__price');
+        priceTab.textContent = `$${priceSize + priceAdd + (+startPrice)}`;
+    });
+});
+
+
+addBtns.forEach(addBtn => {
+    addBtn.addEventListener('click', () => {
+        if (addBtn.classList.contains('active')) {
+            addBtn.classList.remove('active');
+            priceAdd -= 0.5;
+        } else {
+            addBtn.classList.add('active');
+            priceAdd += 0.5;
+        }
+        let priceTab = document.querySelector('.modal-window__price');
+        priceTab.textContent = `$${priceSize + priceAdd + (+startPrice)}`;
+    });
+});
+
+function deletePriceAddictives() {
+    addBtns.forEach(addBtn => {
+        addBtn.classList.remove('active');
+    });
+
+    sizeBtns.forEach(sizeBtn => {
+        sizeBtn.classList.remove('active');
+        sizeBtns[0].classList.add('active');
+    });
+}
